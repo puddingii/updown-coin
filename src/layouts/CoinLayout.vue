b299466d2310ac3ece188d9bd06ccc50ead8bcaa
puddingii <gun4930@naver.com>
2024-04-05 16:23:22 +0900

    chore(layout): 기본 레이아웃 헤더 변경, 푸터 삭제

--- src/layouts/CoinLayout.vue
+++ src/layouts/CoinLayout.vue
@@ -2,34 +2,41 @@
 	<q-layout view="hHh lpR fFf">
 		<q-header elevated class="bg-primary text-white">
 			<q-toolbar>
-				<q-toolbar-title>
-					<q-avatar>
-						<img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg" />
-					</q-avatar>
-					업 & 다운 드개재~
-				</q-toolbar-title>
+				<q-avatar>
+					<img :src="Icon" />
+				</q-avatar>
+
+				<q-toolbar-title>업 & 다운 드개재~</q-toolbar-title>
+
+				<q-btn-dropdown color="secondary" label="메뉴">
+					<q-list>
+						<q-item clickable v-close-popup @click="onClickBack">
+							<q-item-section>
+								<q-item-label>코인 고르기</q-item-label>
+							</q-item-section>
+						</q-item>
+					</q-list>
+				</q-btn-dropdown>
 			</q-toolbar>
 		</q-header>
 
 		<q-page-container>
 			<router-view />
 		</q-page-container>
-
-		<q-footer bordered class="bg-grey-8 text-white">
-			<q-toolbar>
-				<q-toolbar-title>
-					<q-avatar>
-						<img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg" />
-					</q-avatar>
-					<div>업 & 다운 드개재~</div>
-				</q-toolbar-title>
-			</q-toolbar>
-		</q-footer>
 	</q-layout>
 </template>
 
 <script setup lang="ts">
+import { useRouter } from 'vue-router';
+import Icon from 'src/assets/icon.png';
+
 defineOptions({
 	name: 'CoinLayout',
 });
+
+const router = useRouter();
+
+const onClickBack = () => {
+	router.push('/coin');
+};
 </script>
